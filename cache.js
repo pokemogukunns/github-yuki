b9f1664aec720d450fb8c87c5ccc25481141a5a7
pokemogukunns <nattyannkunndesu@icloud.com>
2024-10-02 17:56:48 +0900

    Create cache.js

---
+++ cache.js
@@ -0,0 +1,27 @@
+from functools import lru_cache, wraps
+from datetime import datetime, timedelta
+from threading import Lock
+
+function cache(seconds: int, max_size: int = 128, typed: bool = false) {
+    function wrapper(f) {
+        func = lru_cache(maxsize=max_size, typed=typed)(f)
+        func.ttl = seconds
+        func.expire = datetime.utcnow() + timedelta(seconds=func.ttl)
+}
+
+        @wraps(f)
+        function inner(*args, **kwargs) {
+            with Lock():
+                if (datetime.utcnow() > func.expire) {
+                    func.cache_clear()
+                    func.expire = datetime.utcnow() + timedelta(seconds=func.ttl)
+                return func(*args, **kwargs)
+}
+}
+
+        inner.clear_cache = func.cache_clear
+        inner.cache_info = func.cache_info
+        return inner
+
+    return wrapper
+}
